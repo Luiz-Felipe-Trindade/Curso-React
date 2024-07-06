9dc5d276a9610ccf234b3c3193901da8a692945c
Luiz Felipe <luiz.f.trindade0@gmail.com>
2024-07-05 21:41:57 -0300

    Avancado First Commit

---
+++ avancando/avancando/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>Hello world</p>
+      <img src="/image.png" alt="City" />
+      <p>hello world 2</p>
+    </>
+  );
+}
+
+export default App;
